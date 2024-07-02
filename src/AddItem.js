e05568481067eb22ab6858a9f19e1b3a67bd2053
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-07-02 21:44:13 +0700

    first commit

---
+++ src/AddItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  return (
+    <form className="addForm" action="" onSubmit={ handleSubmit }> 
+      <label htmlFor="addItem">Add Item</label>
+      <input 
+        id="AddItem"
+        type="text"
+        autoFocus
+        placeholder="Add Item"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)} />
+      <button type="submit" aria-label="Add Item"><FaPlus /></button>
+    </form>
+  )
+}
+
+export default AddItem
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = ({ length }) => {
+  const today = new Date();
+  return (
+    <footer>
+      <p>{length} List { length === 1 ? "item" : "items"} | copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  )
+}
+
+export default Footer
